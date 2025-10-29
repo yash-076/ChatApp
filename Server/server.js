65145d6cadd3308264c5087b3f0dd35fa8bcf100
Yash Rastogi <yashrastogi955@gmail.com>
2025-10-29 19:12:07 +0530

    server.js updated for vercel

--- Server/server.js
+++ Server/server.js
@@ -60,5 +60,11 @@ app.use((err, req, res, next) => {
 // Connect to MONGODB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT " + PORT));
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, ()=> console.log("Server is running on PORT " + PORT));  
+}
+
+
+// Export server for vercel
+export default server;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from '../context/AuthContext'
 const App = () => {
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgimage.svg')] bg-cover">
+    <div className="bg-[url('/bgimage.svg')] bg-cover">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ? < HomePage /> : <Navigate to="/login" />} />
